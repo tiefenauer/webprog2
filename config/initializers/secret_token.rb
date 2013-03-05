006637ca4a0655b8dfac6f8f23148bf2979bae0d
Daniel Tiefenauer <git@tiefenauer.info>
2013-03-05 13:04:57 +0100

    added rails tree

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webprog2::Application.config.secret_token = 'b304a9b9c6ff8cc88ae2ff5a2720988e9936590133cd2dbc8cf4b6d07838c067d44b238a7c0f0f55c6d7df1274addaee5e0f76963687ddeab1f286049d6727ef'
