23d90cfe69b16efe7f3f12dd115b2a246fa6332e
tiefenauer <git@tiefenauer.info>
2013-05-04 12:04:32 +0200

    Kapitel 10

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130504085547) do
+ActiveRecord::Schema.define(:version => 20130504090720) do
 
   create_table "books", :force => true do |t|
     t.string   "title"
